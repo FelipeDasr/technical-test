c207440c229a019f690f48b1eaef909c1abab03c
FelipeDasr <felipe.heiker07@gmail.com>
2024-06-15 16:14:33 -0400

    feat(auth): new guard

---
+++ src/core/guards/auth.guard.ts
@@ -0,0 +1,37 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.getTokenFromRequest(request);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    const jwtSettings = { secret: process.env.APP_SECRET };
+    const payload = await this.jwtService.verifyAsync(token, jwtSettings);
+
+    if (!payload) {
+      throw new UnauthorizedException();
+    }
+
+    request.user = payload;
+    return true;
+  }
+
+  private getTokenFromRequest(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
