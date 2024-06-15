433e8829e023000e365f73054ab4c98fe68ae588
FelipeDasr <felipe.heiker07@gmail.com>
2024-06-15 13:26:21 -0400

    feat(validators): new  validator schema of user registration

---
+++ src/core/validators/users/userSignup.validator.ts
@@ -0,0 +1,9 @@
+import * as Joi from 'joi';
+
+import { IUserSignupRequest } from '../../../app/dtos/requests/users.requests.dto';
+
+export const userSignupRequestValidator = Joi.object<IUserSignupRequest>({
+  name: Joi.string().max(100).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
