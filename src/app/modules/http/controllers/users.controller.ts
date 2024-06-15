77548b8b2d5e533f35ee8199cd77095b30cefe35
FelipeDasr <felipe.heiker07@gmail.com>
2024-06-15 15:57:29 -0400

    feat: new app endpoint (/user/signin)

--- src/app/modules/http/controllers/users.controller.ts
+++ src/app/modules/http/controllers/users.controller.ts
@@ -1,16 +1,24 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 
-import { IUserSignupRequest } from 'src/app/dtos/requests/users.requests.dto';
+import {
+  IUserSigninRequest,
+  IUserSignupRequest,
+} from 'src/app/dtos/requests/users.requests.dto';
 
 import { ValidatorPipe } from 'src/core/pipes/requestValidator.pipe';
+import { userSigninRequestValidator } from 'src/core/validators/users/userSignin.validator';
 import { userSignupRequestValidator } from 'src/core/validators/users/userSignup.validator';
 
 import { CreateUserUseCase } from 'src/app/useCases/users/createUser.usecase';
+import { GenerateUserAuthToken } from 'src/app/useCases/auth/generateUserAuthToken.usecase';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly createUserUsecase: CreateUserUseCase) {}
+  constructor(
+    private readonly createUserUsecase: CreateUserUseCase,
+    private readonly generateUserAuthToken: GenerateUserAuthToken,
+  ) {}
 
   @Post('signup')
   public async signup(
@@ -20,5 +28,15 @@ export class UsersController {
   ) {
     const newUser = await this.createUserUsecase.execute(body);
     return response.status(201).json(newUser);
+  }
+
+  @Post('signin')
+  public async signin(
+    @Body(new ValidatorPipe(userSigninRequestValidator))
+    body: IUserSigninRequest,
+    @Res() response: Response,
+  ) {
+    const userAuthData = await this.generateUserAuthToken.execute(body);
+    return response.status(200).json(userAuthData);
   }
 }
--- src/core/providers/http.providers.ts
+++ src/core/providers/http.providers.ts
@@ -1,6 +1,13 @@
+import { JwtService } from '@nestjs/jwt';
+
 import { DatabaseModule } from 'src/app/modules/database/database.module';
 
 import { CreateUserUseCase } from 'src/app/useCases/users/createUser.usecase';
+import { GenerateUserAuthToken } from 'src/app/useCases/auth/generateUserAuthToken.usecase';
 
-export const httpProviders = [CreateUserUseCase];
+export const httpProviders = [
+  JwtService,
+  CreateUserUseCase,
+  GenerateUserAuthToken,
+];
 export const httpImports = [DatabaseModule];
