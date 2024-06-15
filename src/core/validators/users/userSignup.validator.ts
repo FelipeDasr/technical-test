6edc01abe2979de58af7a2d91bfeedcd5047a7a8
FelipeDasr <felipe.heiker07@gmail.com>
2024-06-15 13:32:30 -0400

    fix(validators): add a min length to user password

--- src/core/validators/users/userSignup.validator.ts
+++ src/core/validators/users/userSignup.validator.ts
@@ -5,5 +5,5 @@ import { IUserSignupRequest } from '../../../app/dtos/requests/users.requests.dt
 export const userSignupRequestValidator = Joi.object<IUserSignupRequest>({
   name: Joi.string().max(100).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(6).required(),
 });
