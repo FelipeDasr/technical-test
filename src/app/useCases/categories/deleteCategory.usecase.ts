265cd2894f13fb6554025a0e42c27ae900f0659b
FelipeDasr <felipe.heiker07@gmail.com>
2024-06-17 12:21:29 -0400

    feat(categories): new deletion use case

---
+++ src/app/useCases/categories/deleteCategory.usecase.ts
@@ -0,0 +1,37 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+
+import { ICategoryRepository } from 'src/app/dtos/repositories/category.repository.dto';
+
+@Injectable()
+export class DeleteCategoryUseCase {
+  constructor(private readonly categoryRepository: ICategoryRepository) {}
+
+  public async execute(id: number) {
+    await this.checkIfCategoryExists(id);
+
+    const activeProducts =
+      await this.categoryRepository.countActiveProducts(id);
+
+    if (activeProducts > 0) {
+      throw new BadRequestException('Category has active products');
+    }
+
+    await this.categoryRepository.softDelete(id);
+    return { message: 'Category deleted successfully' };
+  }
+
+  public async checkIfCategoryExists(id: number) {
+    const category = await this.categoryRepository.findOne({
+      where: { id },
+      select: ['id'],
+    });
+
+    if (!category) {
+      throw new NotFoundException('Category not found');
+    }
+  }
+}
