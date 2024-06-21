5e8bfbec9498095e76cec261736a108166190975
FelipeDasr <felipe.heiker07@gmail.com>
2024-06-20 22:38:04 -0400

    feat: new app endpoint (GET /purchases/:userPurchaseId)

--- src/app/modules/http/controllers/purchases.controller.ts
+++ src/app/modules/http/controllers/purchases.controller.ts
@@ -1,6 +1,7 @@
 import {
   Controller,
   Get,
+  Param,
   Query,
   Request,
   Res,
@@ -14,14 +15,17 @@ import { IPaginationQuery } from 'src/app/dtos/repositories';
 import { AuthGuard } from 'src/core/guards/auth.guard';
 import { ValidatorPipe } from 'src/core/pipes/requestValidator.pipe';
 import { paginationRequestValidator } from 'src/core/validators/common/pagination.validator';
+import { idValidator } from 'src/core/validators/common/id.validator';
 
 import { ListUserPurchasesUseCase } from 'src/app/useCases/purchases/listUserPurchases.usecase';
+import { GetUserPurchaseDetailsUsecase } from 'src/app/useCases/purchases/getUserPurchaseDetails.usecase';
 
 @Controller('purchases')
 @UseGuards(AuthGuard)
 export class PurchasesController {
   constructor(
     private readonly listUserPurchasesUseCase: ListUserPurchasesUseCase,
+    private readonly getUserPurchaseDetailsUsecase: GetUserPurchaseDetailsUsecase,
   ) {}
 
   @Get()
@@ -37,5 +41,20 @@ export class PurchasesController {
     );
 
     return response.status(200).json(purchases);
+  }
+
+  @Get(':userPurchaseId')
+  async getUserPurchaseDetails(
+    @Request() request: IAuthenticatedRequest,
+    @Param('userPurchaseId', new ValidatorPipe(idValidator))
+    userPurchaseId: number,
+    @Res() response: Response,
+  ) {
+    const purchaseDetails = await this.getUserPurchaseDetailsUsecase.execute(
+      request.user.id,
+      userPurchaseId,
+    );
+
+    return response.status(200).json(purchaseDetails);
   }
 }
